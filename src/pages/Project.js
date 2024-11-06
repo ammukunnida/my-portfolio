8b54f20e68d70892b5b242e429d21eb166d4b1b0
Anaswara M A <anaswaram21@yahoo.com>
2024-11-06 12:23:57 +0530

    ddd

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -13,6 +13,59 @@ function Project() {
             <div className="item"><a href='/contact'>Contact</a></div>
           </div>
         </nav>
+          {/* -- Education item -- */}
+          <div className='edu-item flex-box'>
+          <img src='https://www.istockphoto.com/photo/man-taking-photo-at-tea-plantations-gm949396238-259169448'></img>
+          <div className='about-right'>
+          <h1>Village Tourism</h1>
+          <h2>MSc. Computer Science (2024 - Present)</h2>
+          <p>I am currently doing my Post Graduation in Computer Science from Karyavattam Campus, Trivandrum</p>
+          </div>
+        </div>
+        {/* -- end -- */}
+        <section class="footer">
+      <div class="footer-row">
+      <div class="footer-col">
+          <h4>Anaswara M A</h4>
+          <p>
+            Graduated from Kerala University.
+          </p>
+          <div class="icons">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-github"></i>
+          </div>
+        </div>
+        <div class="footer-col">
+          <h4>Info</h4>
+           <ul class="links">
+            <li><a href="#">Home Page</a></li>
+            <li><a href="#">About Page</a></li>
+            <li><a href="#">Vision Page</a></li>
+            <li><a href="#">Projects Page</a></li>
+            <li><a href="#">Contact Page</a></li>
+          </ul> 
+        </div>
+        <div class="footer-col">
+          <h4>Projects</h4>
+          <ul class="links">
+            <li><a href="#">Cloud security using hybrid cryptography</a></li>
+            <li><a href="#">Village Tourism Website</a></li>
+            
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Contact</h4>
+          <ul class="links">
+            <li><a href="#">Mobile : +91 9074235261</a></li>
+            <li><a href="#">anaswaram21@gmail.com</a></li>
+            
+          </ul>
+        </div>
+        
+      </div>
+      </section>
         </div>
     );
 
